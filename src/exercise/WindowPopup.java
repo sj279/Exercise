b541b9a4f7700d21a90ab26b2eacf28748ecf3ca
Sagar <sagarjogani2020@gmail.com>
2022-12-28 18:21:26 +0530

    line removed

--- src/exercise/WindowPopup.java
+++ src/exercise/WindowPopup.java
@@ -16,12 +16,6 @@ public class WindowPopup {
 		
 		System.out.println("Test Git 10");
 		System.out.println("Test Git 100");
-		System.out.println("Test Git 10000");
-		System.out.println("Test Git 100000");
-		System.out.println("Test Git 1000000");
-		System.out.println("Test Git 10000000");
-		
-		
 	}
 
 }
